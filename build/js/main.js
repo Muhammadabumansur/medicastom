2db808dd0019237922f26132c6449c6e4abe482a
Magomed Kandaev <muhammad-114@mail.ru>
2017-08-27 16:13:37 +0300

    add mega-menu

--- build/js/main.js
+++ build/js/main.js
@@ -1,80 +1,101 @@
 var mainSlider = document.querySelector('.main-slider');
-var mainSliderFlkty = new Flickity( mainSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  pageDots: false,
-  arrowShape: { 
-    x0: 25,
-    x1: 70, y1: 50,
-    x2: 75, y2: 50,
-    x3: 30
-  }
-});
+if(mainSlider) {
+    var mainSliderFlkty = new Flickity( mainSlider, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: true,
+      pageDots: false,
+      arrowShape: { 
+        x0: 25,
+        x1: 70, y1: 50,
+        x2: 75, y2: 50,
+        x3: 30
+      }
+    });
+}
 
 var offersSlider = document.querySelector('.offers-slider');
-var offersSliderFlkty = new Flickity( offersSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  pageDots: false,
-  groupCells: true,
-  arrowShape: { 
-    x0: 25,
-    x1: 70, y1: 50,
-    x2: 75, y2: 50,
-    x3: 30
-  }
-});
+if (offersSlider) {
+    var offersSliderFlkty = new Flickity( offersSlider, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: true,
+      pageDots: false,
+      groupCells: true,
+      arrowShape: { 
+        x0: 25,
+        x1: 70, y1: 50,
+        x2: 75, y2: 50,
+        x3: 30
+      }
+    });
+}
 
 var advantagesSlider = document.querySelector('.advantages-slider');
-var advantagesSliderFlkty = new Flickity( advantagesSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  arrowShape: { 
-    x0: 30,
-    x1: 60, y1: 30,
-    x2: 65, y2: 30,
-    x3: 35
-  }
-});
+if (advantagesSlider) {
+    var advantagesSliderFlkty = new Flickity( advantagesSlider, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: true,
+    arrowShape: { 
+        x0: 30,
+        x1: 60, y1: 30,
+        x2: 65, y2: 30,
+        x3: 35
+    }
+    });
+}
 
 var feedbackSlider = document.querySelector('.feedback-slider');
-var feedbackSliderFlkty = new Flickity( feedbackSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  groupCells: true,
-  prevNextButtons: false
-});
+if (feedbackSlider) {
+    var feedbackSliderFlkty = new Flickity( feedbackSlider, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: true,
+      groupCells: true,
+      prevNextButtons: false
+    });
+}
 
 var clientsSlider = document.querySelector('.clients-slider');
-var clientsSliderFlkty = new Flickity( clientsSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  groupCells: 1,
-  prevNextButtons: false,
-  pageDots: false
-});
+if (clientsSlider) {
+    var clientsSliderFlkty = new Flickity( clientsSlider, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: true,
+      groupCells: 1,
+      prevNextButtons: false,
+      pageDots: false
+    });
+}
 
 var doctorsSlider = document.querySelector('.doctors-slider');
-var doctorsSliderFlkty = new Flickity( doctorsSlider, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  autoPlay: true,
-  prevNextButtons: false
-});
+if (doctorsSlider) {
+    var doctorsSliderFlkty = new Flickity( doctorsSlider, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: true,
+      prevNextButtons: false
+    });
+}
+
+var megaMenu = document.getElementById('megaMenu');
+var megaMenuOpener = document.getElementById('megaMenuOpener');
+var openMegaMenu = function(e) {
+    e.preventDefault();
+    megaMenu.classList.toggle('closed');
+}
+
+megaMenuOpener.addEventListener('click', openMegaMenu)
